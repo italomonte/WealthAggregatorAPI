2e032710cdeb866de203b01fcc8fd0dd10281467
Ítalo Monte <italopessoal@outlook.com>
2025-02-06 14:52:49 -0400

    feat: creating UserController
    - createUser
    - getUserById

---
+++ src/main/java/dev/italomonte/agregadordeinvestimentos/controller/UserController.java
@@ -0,0 +1,32 @@
+package dev.italomonte.agregadordeinvestimentos.controller;
+
+import dev.italomonte.agregadordeinvestimentos.entity.User;
+import dev.italomonte.agregadordeinvestimentos.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody CreateUserDto createUserDto) {
+        var userId = userService.createUser(createUserDto);
+        return ResponseEntity.created(URI.create("/v1/users/" + userId.toString())).build();
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUserById(@PathVariable("userId") String userId) {
+
+        return null;
+    }
+
+}
